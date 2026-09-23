eaa26ca4fd460be190b9bd67305771bceef4525b
iooimc <iooistudio@proton.me>
2026-09-23 18:14:56 +0200

    Add N3X Forge cape menu entrypoint

---
+++ tools/forge-cape-menu/src/main/java/dev/n3x/capes/menu/N3XCapeMenu.java
@@ -0,0 +1,9 @@
+package dev.n3x.capes.menu;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(N3XCapeMenu.MOD_ID)
+public final class N3XCapeMenu {
+    public static final String MOD_ID = "n3xcapemenu";
+    public N3XCapeMenu() { }
+}
